1b35f5679fd2113987b1d59ba6dbc0b7906b1011
Ryan Vice <ryan@vicesoftware.com>
2020-03-28 15:09:13 -0500

    Updated useEffect to better handle dependencies

--- webapp/src/infrastructure/useAsync.js
+++ webapp/src/infrastructure/useAsync.js
@@ -1,13 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
-import dispatchAsync from './dispatchAsync'
+import buildDispatchAsync from './buildDispatchAsync'
 
-export default function useAsync({ url, actionType, selector }) {
+export default function useAsync({
+	url,
+	actionType,
+	selector,
+	dummyResponse,
+	dummyError,
+	dependencies = [],
+} = {}) {
+	if (
+		dependencies &&
+		(dependencies.length === undefined || dependencies.length === null)
+	) {
+		throw new Error('dependencies must be an array.')
+	}
+
 	const dispatch = useDispatch()
 
+	console.log(
+		`useAsync called for ${url} with depedencies ${JSON.stringify({
+			url,
+			actionType,
+			selector,
+			dummyResponse,
+			dummyError,
+			dependencies,
+		})}`
+	)
+
+	const dispatchAsync = buildDispatchAsync(dispatch)
+
 	useEffect(() => {
-		dispatchAsync({ url, actionType, dispatch })
-	}, [dispatch, actionType, url])
+		dispatchAsync({ url, actionType, dummyResponse, dummyError })
+	}, [url, actionType, selector, dummyResponse, dummyError, ...dependencies])
 
 	return useSelector(selector)
 }
