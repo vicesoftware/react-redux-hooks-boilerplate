c6a1cbb95b8fc533b66fe2e558e1f190de9c3174
Ryan Vice <ryan@vicesoftware.com>
2020-04-03 08:09:44 -0500

    Backed out some business logic to get things back to a working state

--- webapp/src/features/screenTimeReports/screenTimeReports.selectors.js
+++ webapp/src/features/screenTimeReports/screenTimeReports.selectors.js
@@ -1,10 +1,9 @@
 export const getAllScreenTimeReports = (state) =>
 	state.screenTimeReports.allScreenTimeReports
-export const getScreenTimeReportById = (date) => (state) =>
-	state.screenTimeReports.allScreenTimeReports.find((item) => {
-		debugger
-		return new Date(item.date).toDateString() === new Date(date).toDateString()
-	})
+export const getScreenTimeReportById = (screenTimeReportsId) => (state) =>
+	state.screenTimeReports.allScreenTimeReports.find(
+		(item) => item.id === screenTimeReportsId
+	)
 
 const emptyReports = []
 export const getScreenTimeReportsByUserId = (userId) => (state) => {
