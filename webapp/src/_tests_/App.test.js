781ba2c63ee8167d3d709b484966a82c9c04e056
Ryan Vice <ryan@vicesoftware.com>
2020-05-06 07:53:16 -0500

    Added dummy reducer for debugging issue

--- webapp/src/_tests_/App.test.js
+++ webapp/src/_tests_/App.test.js
@@ -4,8 +4,20 @@ import { combineReducers, createStore } from 'redux'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
+const initialState = {}
+
+function reducer(state, action) {
+	if (typeof state === 'undefined') {
+	  return initialState
+	}
+  
+	// For now, don't handle any actions
+	// and just return the state given to us.
+	return state
+  }
+
 it('renders welcome message', () => {
-	const store = createStore(combineReducers({}))
+	const store = createStore(combineReducers({ foos: reducer }))
 
 	const { getByText } = render(
 		<Provider store={store}>
